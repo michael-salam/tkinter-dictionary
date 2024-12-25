2fe736f1c065cbb63d50181c04a49fa94fed2143
Michael Salam <michaeldimimu@gmail.com>
2024-12-25 22:50:46 +0100

    Remove extra dictionary files

---
+++ dictionaries.py
@@ -0,0 +1,25 @@
+# All dictionaries are to be created here
+# Simply add your dictionary to the file
+
+yoruba_dictionary = {
+    "wa": "come",
+    "lo": "go",
+    "joko": "sit",
+    "benni": "yes",
+    "ori": "head",
+    "owo": "money",
+    "ile": "house",
+    "ounje": "food",
+    "omi": "water",
+    "obirin": "woman",
+    "iya": "mother",
+    "oko": "car",
+    "aaye": "space",
+    "oju": "eye",
+    "eti": "ear",
+    "eniyan": "person",
+    "ilekun": "door",
+    "aso": "clothes",
+    "ile": "home",
+    "awo": "plate"
+}
